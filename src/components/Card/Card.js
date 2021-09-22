71350ee9e686d3868e352883d0fed0b5779dc557
Hayley Witherell <hayleywitherell@gmail.com>
2021-09-22 10:28:25 -0600

    Format and round info

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -3,13 +3,15 @@ import React from 'react';
 import './Card.css';
 
 const Card = ({ name, street, city, state, distance }) => {
+
+  const distanceRounded = distance.toFixed(2);
+  
   return (
     <div className='card'>
-      <h3>{name}</h3>
+      <h3 className='businessName'>{name}</h3>
       <p>{street}</p>
-      <p>{city}</p>
-      <p>{state}</p>
-      <p>{distance}</p>
+      <p>{city}, {state}</p>
+      <p>{distanceRounded} mi</p>
     </div>
   )
 }
