9370bda7e0a96a28673a5a91b6b66508f1dffc69
Hayley Witherell <hayleywitherell@gmail.com>
2021-09-22 11:21:23 -0600

    Fix spacing, caps, etc

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -5,12 +5,14 @@ import './Card.css';
 const Card = ({ name, street, city, state, distance }) => {
 
   const distanceRounded = distance.toFixed(2);
+  const cityNoSpace = city.split(' ').join('')
+  const stateCaps = state.toUpperCase()
   
   return (
     <div className='card'>
-      <h3 className='businessName'>{name}</h3>
-      <p>{street}</p>
-      <p>{city}, {state}</p>
+      <h3 className='businessName capitalize'>{name}</h3>
+      <p className='capitalize'>{street}</p>
+      <p className='capitalize'>{cityNoSpace}, {stateCaps}</p>
       <p>{distanceRounded} mi</p>
     </div>
   )
