9922d88ff77850d718216ed7bd313a62511ce74a
Hayley Witherell <hayleywitherell@gmail.com>
2021-09-23 12:29:34 -0600

    Create FAQ files

---
+++ src/components/FAQ/FAQ.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './FAQ.css';
+import Header from '../Header/Header';
+
+const FAQ = () => {
+
+  return (
+    <div>    
+      <Header />
+      <article>test</article>
+    </div>
+)
+}
+
+export default FAQ;
