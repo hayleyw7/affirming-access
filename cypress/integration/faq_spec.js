0515b03d79aa85aa11282921c9d7e046bbcfc591
Hayley Witherell <hayleywitherell@gmail.com>
2021-09-24 14:19:19 -0600

    Complete faq tests

--- cypress/integration/faq_spec.js
+++ cypress/integration/faq_spec.js
@@ -8,6 +8,31 @@ describe('FAQ Page', () => {
     cy.url().should('eq', 'http://localhost:3000/faq')
   })
 
-  
+  it('should render header', () => {
+    cy.get('h3')
+      .contains('FAQ')
+  })
+
+  it('should render questions', () => {
+    cy.get('p')
+      .contains('What is this app')
+  }) 
+
+  it('should render answers', () => {
+    cy.get('p')
+      .contains('had been denied restroom access')
+  })    
+
+  it('should display return home button', () => {
+    cy.get('button[alt="Find Safe Restrooms Now"]')
+      .should('be.visible')
+  })    
+
+  it('should go to search page when home button clicked', () => {
+    cy.get('button[alt="Find Safe Restrooms Now"]')
+      .click()
+    .get('input[alt="Enter Zip Code"]')
+      .should('be.visible')
+  })  
 
 })
