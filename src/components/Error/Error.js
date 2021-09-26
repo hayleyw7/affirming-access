56b56e60e040533ada6e3041a46cadb8ab7522c4
Hayley Witherell <hayleywitherell@gmail.com>
2021-09-26 17:04:14 -0600

    Refactor Error

--- src/components/Error/Error.js
+++ src/components/Error/Error.js
@@ -2,16 +2,17 @@ import React from 'react'
 import './Error.css';
 import errorIcon from './error.png'
 
-const Error = ({ message, page }) => {
+const Error = () => {
   return (
     <div className='error-container'>
-      <h2 className='error-heading'>We've encountered an error in retrieving the {page}.</h2>
-      <p className='error-message'>{message}</p>
+      <h2 className='error-heading'>
+        We've encountered an error in retrieving the page.
+      </h2>
       <img
         alt='Error icon'
         className='error-icon'
         src={errorIcon}
-      ></img>   
+      />  
     </div>
   )
 }
