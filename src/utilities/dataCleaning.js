dc956a5b805a8909f931c9a5c7276ec117769e8c
Hayley Witherell <hayleywitherell@gmail.com>
2021-09-26 14:43:53 -0600

    Format name

--- src/utilities/dataCleaning.js
+++ src/utilities/dataCleaning.js
@@ -4,12 +4,13 @@ export const cleanRestroomsData = (data) => {
 
   return data.map(restroom => {
     return {
-      name: restroom.name,
       city: restroom.city.split(' ').join(''),
       state: normalize(restroom.state),
       unisex: restroom.unisex,
       distance: restroom.distance.toFixed(2),
       id: restroom.id,
+
+      name: restroom.name.split(', USA').join('').split(restroom.city).join('').split(restroom.zip).join('').split(restroom.state).join('').split(normalize(restroom.state)).join('').split(',').join(''),
 
       street: restroom.street.toLowerCase().split('north ').join('N ').split('south ').join('S ').split('east ').join('E ').split('west ').join('W ').split('treet').join('t').split('venue').join('ve').split(' road').join(' Rd').split('oulevard').join('lvd').split('rive').join('r').split('door').join('Dr').split('arkway').join('kwy').split('and').join('&').split('.').join('')
     }
