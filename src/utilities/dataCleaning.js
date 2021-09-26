6aefa12a966bc5127e5c85b5b95909d8064fa799
Hayley Witherell <hayleywitherell@gmail.com>
2021-09-26 13:49:29 -0600

    Fix unique key issue

--- src/utilities/dataCleaning.js
+++ src/utilities/dataCleaning.js
@@ -9,6 +9,7 @@ export const cleanRestroomsData = (data) => {
       state: normalize(restroom.state),
       unisex: restroom.unisex,
       distance: restroom.distance.toFixed(2),
+      id: restroom.id,
 
       street: restroom.street.toLowerCase().split('north ').join('N ').split('south ').join('S ').split('east ').join('E ').split('west ').join('W ').split('treet').join('t').split('venue').join('ve').split(' road').join(' Rd').split('oulevard').join('lvd').split('rive').join('r').split('door').join('Dr').split('arkway').join('kwy').split('and').join('&').split('.').join('')
 
